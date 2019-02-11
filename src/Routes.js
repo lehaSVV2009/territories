68290650d09f2690814778c0a418b1b415444268
Alex Soroka <lehaSVV2009@gmail.com>
2019-02-11 09:55:42 +0530

    Improve first load performance by react-loadable

--- src/Routes.js
+++ src/Routes.js
@@ -1,21 +1,42 @@
 import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
-import AiPage from "./AiPage";
-import HomePage from "./HomePage";
 import ErrorPage from "./Error";
-import OnlineLobbyPage from "./OnlineLobbyPage";
-import TwoPlayersPage from "./TwoPlayersPage";
+import Loadable from "react-loadable";
+import LinearProgress from "./libs/territories-ui/LinearProgress";
+
+const Loading = props =>
+  props.error ? <ErrorPage /> : <LinearProgress color="secondary" />;
+
+const LoadableHomePage = Loadable({
+  loader: () => import("./HomePage"),
+  loading: Loading
+});
+
+const LoadableAiPage = Loadable({
+  loader: () => import("./AiPage"),
+  loading: Loading
+});
+
+const LoadableTwoPlayersPage = Loadable({
+  loader: () => import("./TwoPlayersPage"),
+  loading: Loading
+});
+
+const LoadableOnlinePage = Loadable({
+  loader: () => import("./OnlineLobbyPage"),
+  loading: Loading
+});
 
 // TODO add react-loadable
 const Routes = () => (
   // Hash router is used because of github-pages issues with browser router
   <Router>
     <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/ai" component={AiPage} />
-      <Route path="/two-players" component={TwoPlayersPage} />
-      <Route path="/online" component={OnlineLobbyPage} />
+      <Route exact path="/" component={LoadableHomePage} />
+      <Route path="/ai" component={LoadableAiPage} />
+      <Route path="/two-players" component={LoadableTwoPlayersPage} />
+      <Route path="/online" component={LoadableOnlinePage} />
       <Route path="*" component={ErrorPage} />
     </Switch>
   </Router>
